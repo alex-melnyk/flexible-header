87580fd4e7ba9e6e4dcaf4b44152a9d7f8394125
Alex Melnyk <ask.melnyk@gmail.com>
2020-02-06 18:44:46 +0200

    Cards styles added.

--- App.tsx
+++ App.tsx
@@ -1,115 +1,169 @@
-import React, { ReactElement, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import {
   Animated,
-  Dimensions,
-  FlatList,
+  Dimensions, FlatList,
+  ImageBackground,
   NativeScrollEvent,
   NativeSyntheticEvent,
+  SafeAreaView,
   StyleSheet,
+  Text,
   View
 } from 'react-native';
-import BGImage from './assets/img/bg.jpg';
+
+const BGImage = require('./assets/img/bg.jpg');
 
 const {
-  width: screenWidth
+  width: screenWidth,
+  height: screenHeight
 } = Dimensions.get('screen');
 
-const HEADER_MIN_HEIGHT = 120;
-const HEADER_MAX_HEIGHT = 300;
+const HEADER_MIN_HEIGHT = 80;
+const HEADER_MAX_HEIGHT = screenHeight / 3;
+const HEADER_MAX_INTERPOLATE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+
+const DATA: Array<{
+  label: string;
+  image?: string;
+}> = [
+  {
+    label: 'Everest',
+    image: require('./assets/img/mountains/everest.jpg')
+  },
+  {
+    label: 'K2',
+    image: require('./assets/img/mountains/k2.jpg')
+  },
+  {
+    label: 'Kangchenjunga',
+    image: require('./assets/img/mountains/kangchenjunga.jpg')
+  },
+  {
+    label: 'Lhotse',
+    image: require('./assets/img/mountains/lhotse.jpg')
+  },
+  {
+    label: 'Makalu',
+    image: require('./assets/img/mountains/makalu.jpg')
+  },
+  {
+    label: 'Cho Oyu',
+    image: require('./assets/img/mountains/cho-oyu.jpg')
+  },
+  {
+    label: 'Dhaulagiri',
+    image: require('./assets/img/mountains/dhaulagiri.jpg')
+  },
+  {
+    label: 'Manaslu',
+    image: require('./assets/img/mountains/manaslu.jpg')
+  },
+  {
+    label: 'Nanga Parbat',
+    image: require('./assets/img/mountains/nanga-parbat.jpg')
+  },
+  {
+    label: 'Annapurna',
+    image: require('./assets/img/mountains/annapurna.jpg')
+  }
+];
 
 export default function App() {
   const headerAnimated = useRef(new Animated.Value(0)).current;
-  const [momentum, setMomentum] = useState(false);
-  const flatListRef = useRef<{ getNode: () => FlatList<ReactElement> }>();
 
   const handleScrollBegin = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
     const { nativeEvent: { contentOffset } } = e;
-
-    e.stopPropagation();
-    // console.log('contentOffset.y', contentOffset.y);
     headerAnimated.setValue(contentOffset.y);
   };
 
-  const resolveScrollPosition = (y: number) => {
-    const flatList = flatListRef.current.getNode();
-    console.log('flatListRef.current', y);
-
-    if (y < (HEADER_MAX_HEIGHT / 2)) {
-      flatList.scrollToOffset({
-        animated: true,
-        offset: y < (HEADER_MAX_HEIGHT / 2) ? 0 : 0
-      });
-    }
-  };
-
   const headerHeight = headerAnimated.interpolate({
-    inputRange: [-HEADER_MIN_HEIGHT, 0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+    inputRange: [-HEADER_MIN_HEIGHT, 0, HEADER_MAX_INTERPOLATE],
     outputRange: [HEADER_MAX_HEIGHT + HEADER_MIN_HEIGHT, HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
     extrapolate: 'clamp'
   });
 
+  const headerBg = headerAnimated.interpolate({
+    inputRange: [0, HEADER_MAX_INTERPOLATE],
+    outputRange: ['#393E4100', '#393E419B'],
+    extrapolate: 'clamp'
+  });
+
   const reverseHeight = headerAnimated.interpolate({
-    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+    inputRange: [0, HEADER_MAX_INTERPOLATE],
     outputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
     extrapolate: 'clamp'
   });
 
   const offsetHeight = headerAnimated.interpolate({
-    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+    inputRange: [0, HEADER_MAX_INTERPOLATE],
     outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+    extrapolate: 'clamp'
+  });
+
+  const titleSize = headerAnimated.interpolate({
+    inputRange: [0, HEADER_MAX_INTERPOLATE],
+    outputRange: [28, 22],
     extrapolate: 'clamp'
   });
 
   return (
     <View style={styles.container}>
       <Animated.FlatList
-        ref={flatListRef}
         style={{
           marginTop: offsetHeight
         }}
+        contentContainerStyle={styles.list}
         ListHeaderComponent={(
           <Animated.View style={{ height: reverseHeight }}/>
         )}
         showsVerticalScrollIndicator={false}
         scrollEventThrottle={16}
-        data={[...new Array(10)]}
+        data={DATA}
         keyExtractor={(v, i) => `card_${i}`}
         renderItem={({ item, index: i }) => (
-          <View
-            style={{
-              margin: 10,
-              height: 150,
-              borderRadius: 8,
-              backgroundColor: '#3E2F5B'
-            }}
-          />
+          <View style={styles.item}>
+            <ImageBackground
+              style={styles.itemBackgroundImage}
+              source={item.image}
+            >
+              <View style={styles.itemContent}>
+                <Text style={styles.itemLabel}>
+                  {item.label}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
         )}
         onScroll={handleScrollBegin}
-        onMomentumScrollBegin={() => setMomentum(true)}
-        onMomentumScrollEnd={({ nativeEvent: { contentOffset: { y } } }) => {
-          resolveScrollPosition(y);
-          setMomentum(false);
-        }}
-        onScrollDragEnd={({ nativeEvent: { contentOffset: { y } } }) => {
-          if (!momentum) {
-            resolveScrollPosition(y);
-          }
-        }}
       />
       <Animated.View
-        style={{
-          position: 'absolute',
+        style={[styles.headerContainer, {
           height: headerHeight
-        }}
+        }]}
       >
         <Animated.Image
           source={BGImage}
-          style={{
-            height: headerHeight,
-            width: screenWidth,
-            resizeMode: 'cover'
-          }}
+          style={[styles.headerBackgroundImage, {
+            height: headerHeight
+          }]}
         />
+        <Animated.View
+          style={[styles.headerContentWrapper, {
+            backgroundColor: headerBg
+          }]}
+        >
+          <SafeAreaView
+            style={styles.headerContentContainer}
+          >
+            <Animated.Text
+              style={[styles.headerContentTitle, {
+                fontSize: titleSize
+              }]}
+            >
+              Mountains
+            </Animated.Text>
+          </SafeAreaView>
+        </Animated.View>
       </Animated.View>
     </View>
   );
@@ -119,4 +173,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  list: {
+    // flexGrow: 1,
+    paddingBottom: 10,
+  },
+  headerContainer: {
+    position: 'absolute',
+    width: '100%'
+  },
+  headerBackgroundImage: {
+    position: 'absolute',
+    width: screenWidth,
+    resizeMode: 'cover',
+  },
+  headerContentContainer: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  headerContentWrapper: {
+    padding: 20,
+    flex: 1
+  },
+  headerContentTitle: {
+    fontWeight: '800',
+    textTransform: 'uppercase',
+    color: '#F6F7EB'
+  },
+  item: {
+    margin: 10,
+    marginBottom: 0,
+    height: 200,
+    borderRadius: 10,
+    backgroundColor: '#434343',
+    overflow: 'hidden'
+  },
+  itemBackgroundImage: {
+    width: '100%',
+    height: '100%'
+  },
+  itemContent: {
+    padding: 20,
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  itemLabel: {
+    fontSize: 26,
+    fontWeight: '800',
+    color: '#F6F7EB'
+  }
 });
